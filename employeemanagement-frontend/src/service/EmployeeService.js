fdcfb468d7153eb6c7bd6fd8a3f0eccba35e01b2
MKundargi <122876004+MKundargi@users.noreply.github.com>
2023-10-18 12:52:21 +0530

    initial commit

---
+++ employeemanagement-frontend/src/service/EmployeeService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/employee";
+// const BASE_URL = "http://192.168.49.2:31545/employee";
+class EmployeeService{
+
+    //**Method to get all employee from our api or database */
+    getAllEmployee(){
+        return axios.get(BASE_URL);
+    }
+    /**MEthod to save employee */
+    saveEmployee(employeeData){
+        return axios.post(BASE_URL, employeeData);
+    }
+    updateEmployee(id, employeeData){
+        return axios.put(`${BASE_URL}/${id}`, employeeData)
+    }
+    getEmployeeById(id){
+        return axios.get(`${BASE_URL}/${id}`);
+    }
+    deleteEmployee(id){
+        return axios.delete(BASE_URL +"/" +id);
+    }
+
+}
+export default new EmployeeService();
